0cf6c32c072c6cca34ee588e4794d8c9b4f1993f
Matt Crecraft <mcrecraft@gmail.com>
2022-05-17 16:45:42 -0500

    Solution

--- dnd-character.ts
+++ dnd-character.ts
@@ -1,1 +1,20 @@
-export class DnDCharacter {}
+export class DnDCharacter {
+    strength: number;
+    dexterity: number;
+    constitution: number;
+    intelligence: number;
+    wisdom: number;
+    charisma: number;
+    hitpoints: number;
+
+    constructor() {
+        this.strength = DnDCharacter.generateAbilityScore();
+        this.dexterity = DnDCharacter.generateAbilityScore();
+        this.constitution = DnDCharacter.generateAbilityScore();
+        this.intelligence = DnDCharacter.generateAbilityScore();;
+        this.wisdom = DnDCharacter.generateAbilityScore();
+        this.charisma = DnDCharacter.generateAbilityScore();
+        this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution)
+    }
+
+}
